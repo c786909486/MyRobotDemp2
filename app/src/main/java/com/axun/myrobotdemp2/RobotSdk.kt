399df15bb8b714dd11bea85bc2d5452cf1536900
kzcai <18869978285@163.com>
2021-03-19 19:09:37 +0800

    10

--- app/src/main/java/com/axun/myrobotdemp2/RobotSdk.kt
+++ app/src/main/java/com/axun/myrobotdemp2/RobotSdk.kt
@@ -237,7 +237,7 @@ class RobotSdk private constructor() {
             if (!isInNavi){
                 return
             }
-            if (currentPositionIndex<positions.size ){
+            if (currentPositionIndex<positions.size-1 ){
 //                inOrder = true
                 currentPositionIndex++
 
